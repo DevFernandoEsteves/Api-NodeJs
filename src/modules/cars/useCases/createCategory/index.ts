de1bc441eef11b101ccee19f04abcde946bbc39a
Fernando Esteves <arquivos.fernandoesteves@gmail.com>
2021-08-29 09:48:46 -0300

    Implementação do TSyringe - Refatoração - Error Shared Container

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,12 +1,12 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository"
-import { CreateCategoryUseCase } from "./CreateCategiryUseCase"
-import { CreateCategoryController } from "./CreateCategoryController"
+// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository"
+// import { CreateCategoryUseCase } from "./CreateCategiryUseCase"
+// import { CreateCategoryController } from "./CreateCategoryController"
 
-export default (): CreateCategoryController => {
+// export default (): CreateCategoryController => {
 
-    const categoriesRepository = new CategoriesRepository()
-    const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository)
-    const createCategiryController = new CreateCategoryController(createCategoryUseCase)
+//     const categoriesRepository = new CategoriesRepository()
+//     const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository)
+//     const createCategiryController = new CreateCategoryController(createCategoryUseCase)
 
-    return createCategiryController
-}
+//     return createCategiryController
+// }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import createCategiryController from '../modules/cars/useCases/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import importCategoryController from '../modules/cars/useCases/importCategory';
 import listCategoryController from '../modules/cars/useCases/listCategory';
 
@@ -10,9 +10,9 @@ const upload = multer({
     dest: "./tmp"
 })
 
-categoriesRoutes.post('/', (request, response) => {
-    return createCategiryController().handle(request, response)
-});
+const createCategoryController = new CreateCategoryController();
+
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get('/', (request, response) => {
     return listCategoryController().handle(request, response)
