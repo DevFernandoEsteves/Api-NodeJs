e00cb7ff67a550fc2dc38a452805171845652f25
Fernando Esteves <fernand.esteves@ypplay.com.br>
2021-10-07 08:32:59 -0300

    :fire: Casos de uso de cadastro de especificação para carro. :fire:

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -2,13 +2,10 @@ import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
 import { Car } from '../infra/typeorm/entities/Car';
 
 interface ICarsRepository {
-    create(data: ICreateCarDTO): Promise<Car>;
-    findByLicensePlate(license_plate: string): Promise<Car>;
-    findAvailable(
-        category_id?: string,
-        brand?: string,
-        name?: string
-    ): Promise<Car>;
+	create(data: ICreateCarDTO): Promise<Car>;
+	findByLicensePlate(license_plate: string): Promise<Car>;
+	findAvailable(category_id?: string, brand?: string, name?: string): Promise<Car>;
+	findById(id: string): Promise<Car>;
 }
 
 export { ICarsRepository };
---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,37 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/In-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
+import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car Specification', () => {
+	beforeEach(() => {
+		carsRepositoryInMemory = new CarsRepositoryInMemory();
+		createCarSpecificationUseCase = new CreateCarSpecificationUseCase(carsRepositoryInMemory);
+	});
+
+	it('shold be able to add a new specification to the car', async () => {
+		expect(async () => {
+			const car_id = '1234';
+			const specifications_id: any = ['54321'];
+
+			await createCarSpecificationUseCase.execute({ car_id, specifications_id });
+		}).rejects.toBeInstanceOf(AppError);
+	});
+
+	it('shold be able to add a new specification to the car', async () => {
+		const car = await carsRepositoryInMemory.create({
+			name: 'Name Car',
+			description: 'Description Car',
+			daily_rate: 100,
+			license_plate: 'ABC-1234',
+			fine_amount: 60,
+			brand: 'Brand',
+			category_id: 'category',
+		});
+		const specifications_id: any = ['54321'];
+
+		await createCarSpecificationUseCase.execute({ car_id: car.id, specifications_id });
+	});
+});
