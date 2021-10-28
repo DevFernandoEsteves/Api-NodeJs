c4aad12bea68d1ac41112e08ad2e65755515aeee
Fernando Esteves <arquivos.fernandoesteves@gmail.com>
2021-10-28 00:28:58 -0300

    :fire: Controller de devolução de carro. :fire:

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -3,13 +3,14 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
     id: string
     user_id: string
 }
 
+@injectable()
 class DevolutionRentalUseCase {
     constructor(
         @inject("RentalsRepository")
@@ -22,7 +23,7 @@ class DevolutionRentalUseCase {
 
     async execute({ id, user_id }: IRequest): Promise<Rental> {
         const rental = await this.rentalsRepository.findById(id)
-        const car = await this.carsRepository.findById(id)
+        const car = await this.carsRepository.findById(rental.car_id)
         const minimum_daily = 1
 
         if (!rental) {
@@ -36,12 +37,17 @@ class DevolutionRentalUseCase {
             this.dateProvider.dateNow()
         )
 
+        if (daily <= 0) {
+            daily = minimum_daily
+        }
+
         const delay = this.dateProvider.compareInDays(
             dateNow,
-            rental.expected_return_date
+            rental.start_date
         )
+
         let total = 0
-        if (daily > 0) {
+        if (delay > 0) {
             const calculate_fine = delay * car.fine_amount
             total = calculate_fine
         }
